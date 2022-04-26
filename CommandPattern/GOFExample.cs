d9845228341fe55d44da12261c09e9c57a5ca5c3
Jeff Roach <roachj@etsu.edu>
2022-04-26 14:48:31 -0400

    Updated to .NET 6

--- CommandPattern/GOFExample.cs
+++ CommandPattern/GOFExample.cs
@@ -3,100 +3,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace CommandPattern.GOF
+namespace CommandPattern.GOF;
+
+public abstract class Command
 {
-   public abstract class Command
-   {
-      protected readonly Application _app;
+  protected readonly Application _app;
 
-      public Command(Application app)
-      {
-         _app = app;
-      }
-      
-      public abstract void Execute();
-   }
+  public Command(Application app)
+  {
+     _app = app;
+  }
+  
+  public abstract void Execute();
+}
 
-   public class OpenCommand : Command
-   {
-      public OpenCommand(Application app) : base(app)
-      {
-      }
+public class OpenCommand : Command
+{
+  public OpenCommand(Application app) : base(app)
+  {
+  }
 
-      public override void Execute()
-      {
-         string filename = _app.AskUser("Enter the filename to open: ");
-         Document document = new Document(filename);
-         _app.Add(document);
-      }
-   }
+  public override void Execute()
+  {
+     string filename = _app.AskUser("Enter the filename to open: ");
+     Document document = new Document(filename);
+     _app.Add(document);
+  }
+}
 
-   public class DeleteCommand : Command
-   {
-      public DeleteCommand(Application app) : base(app)
-      {
-      }
+public class DeleteCommand : Command
+{
+  public DeleteCommand(Application app) : base(app)
+  {
+  }
 
-      public override void Execute()
-      {
-         string filename = _app.AskUser("Enter the filename to delete: ");
-         string answer = _app.AskUser("Are your sure (Y/N)? ");
-         if(answer == "Y")
-         {
-            _app.Delete(filename);
-         }
-      }
-   }
+  public override void Execute()
+  {
+     string filename = _app.AskUser("Enter the filename to delete: ");
+     string answer = _app.AskUser("Are your sure (Y/N)? ");
+     if(answer == "Y")
+     {
+        _app.Delete(filename);
+     }
+  }
+}
 
-   public class ShowAllCommand : Command
-   {
-      public ShowAllCommand(Application app) : base(app)
-      {
-      }
+public class ShowAllCommand : Command
+{
+  public ShowAllCommand(Application app) : base(app)
+  {
+  }
 
-      public override void Execute()
-      {
-         foreach(Document document in _app.Documents)
-         {
-            Console.WriteLine(document.FileName);
-         }
-      }
-   }
+  public override void Execute()
+  {
+     foreach(Document document in _app.Documents)
+     {
+        Console.WriteLine(document.FileName);
+     }
+  }
+}
 
-   public class Document
-   {
-      public string FileName { get; }
+public class Document
+{
+  public string FileName { get; }
 
-      public Document(string filename)
-      {
-         FileName = filename;
-      }
-   }
+  public Document(string filename)
+  {
+     FileName = filename;
+  }
+}
 
-   public class Application
-   {
-      public List<Document> Documents { get; private set; }
-         = new List<Document>();
+public class Application
+{
+  public List<Document> Documents { get; private set; }
+     = new List<Document>();
 
-      public string AskUser(string prompt)
-      {
-         Console.Write(prompt);
-         return Console.ReadLine();
-      }
+  public string AskUser(string prompt)
+  {
+     Console.Write(prompt);
+     return Console.ReadLine();
+  }
 
-      public void Add(Document document)
-      {
-         Documents.Add(document);
-      }
+  public void Add(Document document)
+  {
+     Documents.Add(document);
+  }
 
-      internal void Delete(string filename)
-      {
-         Document documentToDelete = 
-            Documents.FirstOrDefault(d => d.FileName == filename);
-         if(documentToDelete != null)
-         {
-            Documents.Remove(documentToDelete);
-         }
-      }
-   }
+  internal void Delete(string filename)
+  {
+     Document documentToDelete = 
+        Documents.FirstOrDefault(d => d.FileName == filename);
+     if(documentToDelete != null)
+     {
+        Documents.Remove(documentToDelete);
+     }
+  }
 }
--- IteratorPattern/IteratorExampleApp.cs
+++ IteratorPattern/IteratorExampleApp.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 
-namespace IteratorPattern
+namespace IteratorPattern;
+
+class IteratorExampleApp
 {
-   class IteratorExampleApp
-   {
-      static void Main(string[] args)
-      {
-         NumberArray array = new NumberArray();
-         array.Fill(100);
+  static void Main(string[] args)
+  {
+     NumberArray array = new NumberArray();
+     array.Fill(100);
 
-         ForwardIterator fit = new ForwardIterator(array);
-         while (!fit.IsDone())
-         {
-            Console.Write($"{fit.Current()} ");
-            fit.Next();
-         }
-         Console.WriteLine();
+     ForwardIterator fit = new ForwardIterator(array);
+     while (!fit.IsDone())
+     {
+        Console.Write($"{fit.Current()} ");
+        fit.Next();
+     }
+     Console.WriteLine();
 
-         ReverseIterator rit = new ReverseIterator(array);
-         while (!rit.IsDone())
-         {
-            Console.Write($"{rit.Current()} ");
-            rit.Next();
-         }
-         Console.WriteLine();
-      }
-   }
+     ReverseIterator rit = new ReverseIterator(array);
+     while (!rit.IsDone())
+     {
+        Console.Write($"{rit.Current()} ");
+        rit.Next();
+     }
+     Console.WriteLine();
+  }
 }
